1402af5cbf8d003f904fc3cafad3e82a32194d06
ChrisMQ <christinamorillo@users.noreply.github.com>
2018-10-19 17:02:56 -0400

    PowerShell Azure AD UPN Script
    PowerShell Script (.ps1) connects to your Azure AD tenant, obtains a list of Azure AD UserPrincipalNames and exports these to a file

---
+++ Azure AD Scripts/Azure-AD-UPNs1.ps1
@@ -0,0 +1,7 @@
+#Login to Azure AD Powershell with Admin Account
+Connect-AzureAD
+
+#Obtain a list of Azure AD users principal names (email addresses) and export this to a file named adupnsoutput.csv in C:\Scripts 
+#YOU MUST CREATE A SCRIPTS FOLDER IN C:\ OR CHANGE FILE PATH LOCATION
+
+get-azureaduser -all:$true| select UserPrincipalName| export-csv -Path "c:\users\rootsadmin\scripts\adupnsoutput1.csv" -notype
