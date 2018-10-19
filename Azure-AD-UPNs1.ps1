1f7085e13f940c17ddd5b98ca98748d9f38801f9
ChrisMQ <christinamorillo@users.noreply.github.com>
2018-10-19 17:27:29 -0400

    Updating files
    Updating file

--- Azure-AD-UPNs1.ps1
+++ Azure-AD-UPNs1.ps1
@@ -4,4 +4,4 @@ Connect-AzureAD
 #Obtain a list of Azure AD users principal names (email addresses) and export this to a file named adupnsoutput.csv in C:\Scripts 
 #YOU MUST CREATE A SCRIPTS FOLDER IN C:\ OR CHANGE FILE PATH LOCATION
 
-get-azureaduser -all:$true| select UserPrincipalName| export-csv -Path "c:\users\rootsadmin\scripts\adupnsoutput1.csv" -notype
+get-azureaduser -all:$true| select UserPrincipalName| export-csv -Path "c:\scripts\adupnsoutput.csv" -notype
